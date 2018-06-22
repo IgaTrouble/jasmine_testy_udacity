48f83c10709ff89c1c2cd85a79f42c324593ee89
Iga <iga.koninska@gmail.com>
2018-06-22 11:15:14 +0200

    defined and not empty url

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,8 +31,16 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
-
+		it('defined URL', function() {
+			allFeeds.forEach(function(feed) {
+				expect(feed.url).toBeDefined;
+			});		
+		});	
+		it('not empty URL', function() {
+			allFeeds.forEach(function(feed) {
+				expect(feed.url.length).not.toBe(0);
+			});			
+		});	
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
